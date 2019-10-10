0c68c1737d6e195a0c6e00c9d5005b81398f7e85
derek young <dereky@hawaii.edu>
2019-10-10 12:51:38 -1000

    re-tabulate, add logic for country detection

--- js/datapage.js
+++ js/datapage.js
@@ -1,20 +1,42 @@
-// start of datapage.js
-
 var URL_pre = ""
 if (DEVELOPMENT) URL_pre = SERVER_URL;
 
+//country = null
 $.ajaxSetup({
   cache: false
 });
 
 function doAjax(dataType) {
-  $.ajax({url: URL_pre + dataType + ".html",
+  $.ajax({url: URL_PRE + dataType + ".html",
   success: function(result){
     $("#sorttable").html(result);
     $('#table').tablesorter( {sortList: [[0,0]]} );
+    //$("#table").on("click", "td", function(e) {console.log(e.currentTarget.parentNode.cells[3].innerText)})
+    //$("#table").on("click", "td", function(e) {country = e.currentTarget.parentNode.cells[3].innerText})
+    //$("#table").on("click", "td", function(e) {disclaimer(e) })
+    $("#table").on("click", "a", function(e) {e.preventDefault();disclaimer(e)})
   }
 });
 };
+
+function disclaimer(clicked) {
+  country = clicked.target.offsetParent.parentElement.cells[3].innerText;
+  link = clicked.currentTarget.href;
+  //console.log(country);
+  //console.log(link);
+  //console.log(clicked);
+  if (country == 'South Africa') {
+    $('#SAModal').modal('show');
+    $('#disclaimerBtn').on('click', function () {
+      $(this).button('complete');
+      window.location = link;
+    })
+  }
+  else {
+    //console.log(country);
+    window.location = link;
+  }
+}
 
 $(document).ready(function() {
 
@@ -33,7 +55,9 @@ $(document).ready(function() {
 
   console.log(datatype);
 
+
   $('a[href*=#]:not([href=#])').click(function() {
+    console.log(country);
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -56,6 +80,7 @@ $(document).ready(function() {
 
     }
   });
+
 
   $('#toTop').click(function() {
     $('html,body').animate({
